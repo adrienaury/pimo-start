3be9dcc503816b97ab6ecc4ce6356fa65bd046ae
adrienaury <adrienaury@gmail.com>
2023-05-07 09:44:00 +0000

    feat: wip! basic form

--- web/start/src/Main.elm
+++ web/start/src/Main.elm
@@ -1,6 +1,52 @@
-module Main exposing (main)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
-import Html exposing (text)
+
+
+-- MAIN
 
 main =
-  text "Hello!"
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+type alias FieldDefinition =
+  { name : String,
+    generator : String
+  }
+
+
+type alias Model = List FieldDefinition
+
+init : Model
+init = [FieldDefinition "test" "test",FieldDefinition "test2" "test2"]
+
+
+
+-- UPDATE
+
+type Msg
+  = AddFieldDefinition
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    AddFieldDefinition ->
+      FieldDefinition "name" "generator" :: model
+
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    (List.map viewFieldDefinition model)
+
+viewFieldDefinition : FieldDefinition -> Html Msg
+viewFieldDefinition f =
+  text f.name
